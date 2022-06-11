11ff1352aa23566e415a0195b407dd5941a3d6ca
login101i <maciejkruszyniak@gmail.com>
2022-06-11 16:41:58 +0200

    AsyncStorage added

--- hooks/useCachedResources.ts
+++ hooks/useCachedResources.ts
@@ -1,6 +1,8 @@
+import { storeData, getData } from './../storage/index';
 
 import { useEffect, useState } from "react";
 import * as Font from "expo-font";
+import { data } from '../data';
 
 export  function useCachedResources() {
   const [isLoadingComplete, setIsLoadingComplete] = useState(false);
@@ -12,9 +14,13 @@ export  function useCachedResources() {
           "montserrat": require("../assets/fonts/Montserrat-Regular.ttf"),
           "montserrat-bold": require("../assets/fonts/Montserrat-Bold.ttf"),
         })
+
+        await storeData("workout-data", data);
       } catch (e) {
         console.warn(e);
       } finally {
+        const workouts = await getData("workout-data");
+        console.log("🚀 ~ file: useCachedResources.ts ~ line 23 ~ loadResourcesAndDataAsync ~ workouts", workouts)
         setIsLoadingComplete(true);
       }
     }
